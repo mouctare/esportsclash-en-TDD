eef63f57eecfdabab7e15d94945f33a30387e8ab
mouctare <mouctard78@gmail.com>
2024-03-14 11:39:45 +0100

    Message de validation des modifications en cours

--- src/main/java/esportsclash/pratique/player/infrastructure/spring/PlayerConfiguration.java
+++ src/main/java/esportsclash/pratique/player/infrastructure/spring/PlayerConfiguration.java
@@ -1,6 +1,5 @@
 package esportsclash.pratique.player.infrastructure.spring;
 
-import esportsclash.pratique.player.infrastructure.persistance.jpa.SQLPlayerDataAccessor;
 import esportsclash.pratique.player.infrastructure.persistance.jpa.SQLPlayerRepository;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
